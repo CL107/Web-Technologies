efebc85d31f1f0a84f8725582c99d802a0a4d45c
Cameron Liddell <camliddell@outlook.com>
2023-04-20 21:36:10 +0100

    working on parsing json from api

--- Scripts/edamam.js
+++ Scripts/edamam.js
@@ -7,18 +7,62 @@ const options = {
 };
 
 // When the user clicks the search button
-document.getElementById("search-button").addEventListener("click", searchFunction());
+document.getElementById("search-button").addEventListener("click", function() {searchFunction()} );
+
+var form=document.getElementById("explore-form");
+function submitForm(event){
+
+   //Preventing page refresh
+   event.preventDefault();
+}
+
+form.addEventListener('submit', submitForm);
 
 function searchFunction() {
     
+    // Get the search input
     var input = document.getElementById("explore-search").value;
-    alert(input);
 
-    fetch('https://edamam-recipe-search.p.rapidapi.com/search?q=chicken', options)
+    // Fetch the recipes from edamam API
+    fetch(`https://edamam-recipe-search.p.rapidapi.com/search?q=${input}`, options)
     .then(response => response.json())
     .then(response => console.log(response))
+    .then(response => createCard(response))
     .catch(err => console.error(err));
+
 }
 
+// Dynamically create a card for each recipe
+function createCard(response) {
+   
+    var response = JSON.parse(response);
+    console.log(response);
 
+    // Get the number of recipes
+    var numRecipes = response[count];
+    console.log(numRecipes);
+   
+    // Get the container div
+    var container = document.getElementById("explore-results");
+   
+    // Create a figure for each recipe
+    var recipeFigure = document.createElement("figure");
+    recipeFigure.id = "recipe-figure";
+    recipeFigure.classList.add("grid-image-container");
+    container.appendChild(recipeFigure);
+    
+    // Retrieve an image for each recipe
+    var recipeImage = document.createElement("img");
+    recipeImage.id = "recipe-image";
+    recipeImage.classList.add("grid-image");
+    recipeImage.src = "/Images/splash.jpg"
+    recipeFigure.appendChild(recipeImage);
+
+    // Retrieve a caption for each recipe
+    var recipeCaption = document.createElement("figcaption");
+    recipeCaption.id = "recipe-caption";
+    recipeCaption.classList.add("caption-centered");
+    recipeCaption.innerHTML = "Recipe Name";
+    recipeFigure.appendChild(recipeCaption);
+}
 
